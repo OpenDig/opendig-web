9fa3bb6ec8d22b24dfcb6572e6e8144a16977c51
Matthew Vincent <mvincent@basecamp.com>
2022-06-20 14:14:55 +0300

    monkey patching a fix

--- config/initializers/couchdb.rb
+++ config/initializers/couchdb.rb
@@ -1,5 +1,15 @@
 require 'couchrest'
 
+class HTTPClient
+  alias original_initialize initialize
+
+  def initialize(*args, &block)
+    original_initialize(*args, &block)
+    # Force use of the default system CA certs (instead of the 6 year old bundled ones)
+    @session_manager&.ssl_config&.set_default_paths
+  end
+end
+
 couchdb_config = YAML.load_file("#{Rails.root}/config/couchdb.yml")[Rails.env]
 protocol = couchdb_config["protocol"]
 host     = couchdb_config["host"]
