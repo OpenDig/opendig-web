f8ce668385c062c6b541a3c2844c5ad1aa2eaa48
Matthew Vincent <mvincent@basecamp.com>
2022-07-30 00:25:38 +0300

    addiing gis ids to field finds

--- app/models/find.rb
+++ app/models/find.rb
@@ -2,15 +2,15 @@ require 'date'
 
 class Find
 
-  attr_accessor :locus, :pail_number, :pail_date, :field_number, :type, :remarks, :id, :season
+  attr_accessor :locus, :pail_number, :pail_date, :field_number, :type, :remarks, :id, :season, :gis_id
 
   def initialize(row)
-    @locus, @pail_number, @pail_date, @field_number, @type, @remarks, @id = row
+    @locus, @pail_number, @pail_date, @field_number, @type, @remarks, @gis_id, @id = row
     @season = Date.parse(@pail_date)&.year || nil
   end
 
   def to_ary
-    [locus, pail_number, pail_date, field_number, type, remarks, id]
+    [locus, pail_number, pail_date, field_number, type, remarks, gis_id, id]
   end
 
 end
