a9b43f3f8b141ac617b058afa76c20b84811bede
Paul Hartman <contact@paulhartman.dev>
2026-01-16 03:31:47 +0000

    Prevent double-login (logging in without logging out first)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def create
+    if user_signed_in?
+      redirect_to root_path, notice: "Already logged in!"
+      return
+    end
+
     auth = request.env['omniauth.auth']
     user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
       u.email = auth['info']['email']
