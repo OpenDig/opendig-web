a074978c1399d0083f6988cc302d53a05d076ad0
Paul Hartman <contact@paulhartman.dev>
2026-02-04 05:09:08 +0000

    Add specs for app authentication

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'Authentication', type: :request do
-  it 'creates a user and signs them in upon successful authentication' do
-    get '/auth/github/callback'
-    expect(response).to redirect_to(root_path) # or wherever you redirect after login
+shared_examples_for 'authentication provider' do |provider|
+  context "- logging in with #{provider}" do
+    it 'creates a user and signs them in upon successful authentication' do
+      get auth_callback_path(provider: provider)
+      expect(response).to redirect_to(root_path)
 
-    follow_redirect!
-    expect(response.body).to include('jane@example.com') # verify the user info appears
-    expect(User.count).to eq(1)
+      follow_redirect!
+      expect(response.body).to include('John Doe') # verify the user info appears
+      expect(User.count).to eq(1)
+    end
+
+    it 'handles authentication failure' do
+      OmniAuth.config.mock_auth[provider.to_sym] = :invalid_credentials # Mock a failure
+
+      get auth_callback_path(provider: provider)
+      expect(response).to redirect_to(auth_failure_path(message: 'invalid_credentials', strategy: provider))
+      follow_redirect!
+      expect(response).to redirect_to(root_path)
+
+      follow_redirect!
+      expect(response.body).to include('Authentication failed')
+    end
   end
+end
 
-  it 'handles authentication failure' do
-    OmniAuth.config.mock_auth[:github] = :invalid_credentials # Mock a failure
-
-    get '/auth/github/callback'
-    expect(response).to redirect_to(root_path)
-
-    follow_redirect!
-    expect(response.body).to include('Authentication failed')
+{
+  'GitHub' => :github,
+  'Google' => :google_oauth2,
+  'Microsoft' => :microsoft_office365
+}.each do |provider_formatted, provider_id|
+  RSpec.describe "#{provider_formatted} authentication", type: :request do
+    it_behaves_like 'authentication provider', provider_id
   end
 end
