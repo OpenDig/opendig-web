e65e60b2d7f60eef02e05c0d4a77e240e578e26d
Paul Hartman <contact@paulhartman.dev>
2026-03-03 17:26:14 +0000

    Simplify AdminController#update_user render logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,13 +10,13 @@ class AdminController < ApplicationController
     if @user.update(user_params)
       respond_to do |format|
         flash.now[:notice] = "User updated successfully"
-        format.html { render partial: 'admin/manage_users_role_dropdown', locals: { user: @user, change: :success }, layout: false }
+        format.html { render partial: 'admin/manage_users_role_dropdown', user: @user, layout: false }
         format.turbo_stream
       end
     else
       respond_to do |format|
         flash.now[:alert] = "Failed to update user"
-        format.html { render partial: 'admin/manage_users_role_dropdown', locals: { user: @user, change: :failure }, layout: false }
+        format.html { render partial: 'admin/manage_users_role_dropdown', user: @user, layout: false }
         format.turbo_stream
       end
     end
